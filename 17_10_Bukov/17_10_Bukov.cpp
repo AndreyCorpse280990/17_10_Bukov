fcc531ce553156e75e7bd56e495f6677f750bf3a
AndreyCorpse28090 <280990corpse@gmail.com>
2023-10-29 09:45:16 +0300

    sort

--- 17_10_Bukov/17_10_Bukov.cpp
+++ 17_10_Bukov/17_10_Bukov.cpp
@@ -1,20 +1,57 @@
-﻿// 17_10_Bukov.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+
+template <typename T>
+void MinMax(T array[], size_t size, T& min, T& max)
+{
+    if(size > 0)
+    {
+        min = max = array[0];
+    }
+
+    for(size_t i = 1; i < size; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+        else if(array[i] > max)
+            max = array[i];
+    }
+}
+template <typename T>
+/* func for sort array. buble*/
+void Sort(T array[], size_t size)
+{
+    for(size_t i = 0; i < size - 1; i++)
+    {
+        for(size_t j = 0; j < size - i - 1; j++)
+        {
+            if(array[j] > array[j + 1])
+            {
+                T temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }  
+        }
+    }
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale (LC_ALL, "rus");
+    int arrayint[] {1, 9, 5, 7, 0, 10, 2, 6, 4, 3, -1};
+    int minint, maxint;
+
+    MinMax(arrayint, sizeof(arrayint) / sizeof(arrayint[0]), minint, maxint);
+    std::cout << "Минимальное значение int : " << minint << std::endl;
+    std::cout << "Максимальное значение int : " << maxint << std::endl;
+
+    double arraydouble[]{2.5, 1.1, 7.6, 0.9, 2.7, 3.5, 4.9};
+    double mindouble, maxdouble;
+
+    MinMax(arraydouble, sizeof(arraydouble) / sizeof(arraydouble[0]), mindouble, maxdouble);
+    std::cout << "Минимальное значение double : " << mindouble << std::endl;
+    std::cout << "Максимальное значение double : " << maxdouble << std::endl;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
